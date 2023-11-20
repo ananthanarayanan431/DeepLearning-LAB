9edcd8f1d41b6ac99a542f29d05f8154521370c5
Anantha Narayanan <125557793+ananthanarayanan431@users.noreply.github.com>
2023-11-20 19:26:12 +0530

    Update 3_OBJECT DETECTION WITH DIGITS.py

--- 3_OBJECT DETECTION WITH DIGITS/3_OBJECT DETECTION WITH DIGITS.py
+++ 3_OBJECT DETECTION WITH DIGITS/3_OBJECT DETECTION WITH DIGITS.py
@@ -90,3 +90,57 @@ def predict_image(model, image):
 # Example usage of the prediction function
 example_image = x_test[0].reshape(1, 28, 28, 1)
 predict_image(model, example_image)
+
+
+
+#After Changes on 20/11/2023 for reducing the no of lines
+
+# import matplotlib.pyplot as plt
+# from keras.models import Sequential
+# from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+# from keras.datasets import mnist
+# from keras.utils import to_categorical
+
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# x_train = x_train.reshape(x_train.shape[0], 28, 28, 1).astype('float32')
+# x_test = x_test.reshape(x_test.shape[0], 28, 28, 1).astype('float32')
+# x_train /= 255
+# x_test /= 255
+
+# y_train = to_categorical(y_train)
+# y_test = to_categorical(y_test)
+
+# print(y_train.shape)
+
+# classes = y_train.shape[1]
+
+# model = Sequential()
+# model.add(Conv2D(16, (5, 5), strides=(1, 1), activation="relu", input_shape=(28, 28, 1)))
+# model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+# model.add(Flatten())
+# model.add(Dense(100, activation="relu"))
+# model.add(Dense(classes, activation="softmax"))
+
+# model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=['accuracy'])
+
+# model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=10, batch_size=200, verbose=2)
+
+# score = model.evaluate(x_test, y_test, verbose=0)
+# print("Accuracy: {}\nError: {}".format(score[1], 100 - score[1] * 100))
+
+# def predict_image(model_func, image):
+#     prediction = model_func.predict(image)
+#     predicted_class = prediction.argmax()
+#     probability = prediction[0, predicted_class]
+
+#     plt.imshow(image.reshape(28, 28), cmap="gray", interpolation="bicubic")
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.title(f"Predicted Class: {predicted_class}, Probability: {probability:.2f}")
+#     plt.show()
+
+
+# # Example usage of the prediction function
+# example_image = x_test[0].reshape(1, 28, 28, 1)
+# predict_image(model, example_image)
