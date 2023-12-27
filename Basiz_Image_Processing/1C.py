c04d26d086320c0d37e41da5c2936259aad5d61b
Anantha Narayanan <125557793+ananthanarayanan431@users.noreply.github.com>
2023-12-27 09:28:49 +0530

    Update 1C.py

--- Basiz_Image_Processing/1C.py
+++ Basiz_Image_Processing/1C.py
@@ -6,3 +6,22 @@ image = image.convert("L")
 image = image.filter(ImageFilter.FIND_EDGES)
 image.save("output.png")
 image.show("output.png")
+
+
+
+
+import cv2 
+  
+img = cv2.imread("anantha.jpg")  # Read image 
+  
+# Setting parameter values 
+t_lower = 50  # Lower Threshold 
+t_upper = 150  # Upper threshold 
+  
+# Applying the Canny Edge filter 
+edge = cv2.Canny(img, t_lower, t_upper) 
+  
+cv2.imshow('original', img) 
+cv2.imshow('edge', edge) 
+cv2.waitKey(0) 
+cv2.destroyAllWindows() 
